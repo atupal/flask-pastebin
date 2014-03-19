1d450c4fda19773a45c5659489425963ace78695
atupal <atupalykl@gmail.com>
2014-03-19 18:45:05 +0800

    stop server before start

--- start.sh
+++ start.sh
@@ -5,5 +5,6 @@ cd $ROOT
 
 git pull
 
+sudo netstat -anp  | grep 8888 | awk ' {split($7, a, "/"); print a[1]}' |xargs sudo kill -9
 sudo pip-2.7 install -r requirements.txt
 sudo python2 manage.py runserver -t 0.0.0.0 -p 8888 -d -r
